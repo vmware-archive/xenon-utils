fc89be1a312da65f3227339f566d9d5162fc4925
Agila Govindaraju <agovindaraju@vmware.com>
2017-01-25 15:50:17 -0800

    Swagger Adapeter Changes
    Note: Code will not compile as the dependency is from 1.3.6. Sending this
    code just for review, so that i can incorporate review comments. Have tested
    locally to generate the swagger spec for TokenService.
    
    - Swagger assembler changes for having custom tag and description for
      services
    - Added support for body and query parameters as part of each of the http
      action for a service.
    
    Change-Id: I66329b9a0c6d66312f92b5cc2ad2f98c2362c15d

--- swagger-adapter/src/test/java/com/vmware/xenon/swagger/TokenService.java
+++ swagger-adapter/src/test/java/com/vmware/xenon/swagger/TokenService.java
@@ -13,18 +13,22 @@
 
 package com.vmware.xenon.swagger;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 
 import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.OperationProcessingChain;
+import com.vmware.xenon.common.RequestRouter;
 import com.vmware.xenon.common.RequestRouter.Route;
 import com.vmware.xenon.common.ServiceDocument;
 import com.vmware.xenon.common.ServiceDocument.UsageOption;
-import com.vmware.xenon.common.ServiceDocumentDescription;
 import com.vmware.xenon.common.ServiceDocumentDescription.PropertyUsageOption;
 import com.vmware.xenon.common.StatelessService;
+import com.vmware.xenon.common.UriUtils;
 
 public class TokenService extends StatelessService {
     public static final String SELF_LINK = "/tokens";
@@ -40,6 +44,37 @@ public class TokenService extends StatelessService {
         public String token;
         @UsageOption(option = PropertyUsageOption.SERVICE_USE)
         public String internalId;
+    }
+
+    @Override
+    public OperationProcessingChain getOperationProcessingChain() {
+        RequestRouter requestRouter = new RequestRouter();
+
+        // Post with activate action activates the pipeline
+        requestRouter.register(
+                Action.GET,
+                new RequestRouter.RequestUriMatcher("type=short"),
+                this::handleGetForShort, "Short version");
+        // Post with execute action executes the pipeline
+        requestRouter.register(
+                Action.GET,
+                new RequestRouter.RequestUriMatcher("type=long"),
+                this::handleGet, "Long version");
+
+        requestRouter.register(Action.PATCH,
+                new RequestRouter.RequestBodyMatcher<>(UserToken.class, "token",
+                        "34bf4c10-e122-11e6-bf01-fe55135034f3"),
+                this::handlePatch, "Patch Token 1");
+
+        requestRouter.register(Action.PATCH,
+                new RequestRouter.RequestBodyMatcher<>(UserToken.class, "token",
+                        "34bf4c10-e122-11e6-bf01-fe55135034f1"),
+                this::handlePatch, "Patch Token 2");
+
+        OperationProcessingChain opProcessingChain = new OperationProcessingChain(this);
+        opProcessingChain.add(requestRouter);
+        setOperationProcessingChain(opProcessingChain);
+        return opProcessingChain;
     }
 
 
@@ -59,27 +94,75 @@ public class TokenService extends StatelessService {
     }
 
     @Override
+    public void handlePut(Operation put) {
+        UserToken body = put.getBody(UserToken.class);
+        Map<String, String> queryParams = UriUtils.parseUriQueryParams(put.getUri());
+        String appendValue = queryParams.get("append");
+
+        UserToken currentState = new UserToken();
+        currentState.token = body.token;
+        currentState.user = body.user;
+        if (Boolean.valueOf(appendValue)) {
+            currentState.internalId = body.user + body.internalId;
+        }
+        log(Level.INFO, "user:%s token:%s internal:%s", currentState.user, currentState.token,
+                currentState.internalId);
+        put.setBody(currentState).complete();
+    }
+
+    @Override
+    public void handlePatch(Operation patch) {
+        UserToken body = patch.getBody(UserToken.class);
+        UserToken currentState = new UserToken();
+        currentState.token = body.token;
+        currentState.user = body.user;
+        currentState.internalId = body.internalId;
+        log(Level.INFO, "user:%s token:%s internal:%s", currentState.user, currentState.token,
+                currentState.internalId);
+        patch.setBody(currentState).complete();
+    }
+
+    @Override
     public ServiceDocument getDocumentTemplate() {
         ServiceDocument d = super.getDocumentTemplate();
-        d.documentDescription = new ServiceDocumentDescription();
 
-        d.documentDescription.serviceRequestRoutes = new HashMap<>();
+        if (d.documentDescription.serviceRequestRoutes == null) {
+            d.documentDescription.serviceRequestRoutes = new HashMap<>();
+        }
+        d.documentDescription.name = "Custom Tag Name";
+        d.documentDescription.description = "Custom Service Description";
 
         Route route = new Route();
-        route.action = Action.GET;
-        route.description = "Retrieves a random token";
-        route.responseType = Token.class;
-
+        route.action = Action.POST;
+        route.description = "Creates user-token mapping";
+        route.requestType = UserToken.class;
         d.documentDescription.serviceRequestRoutes
                 .put(route.action, Collections.singletonList(route));
 
         route = new Route();
-        route.action = Action.POST;
-        route.description = "Creates user-token mapping";
+        route.action = Action.PUT;
+        route.description = "Replace user-token mapping";
         route.requestType = UserToken.class;
+
+        route.parameters = new ArrayList<>();
+        RequestRouter.Parameter parameter1 = new RequestRouter.Parameter("append", "Append Id",
+                "string", false, "true", RequestRouter.ParamDef.QUERY);
+        RequestRouter.Parameter parameter2 = new RequestRouter.Parameter("token", "Token value",
+                "string", false, "34bf4c10-e122-11e6-bf01-fe55135034f3", RequestRouter.ParamDef.BODY);
+        RequestRouter.Parameter parameter3 = new RequestRouter.Parameter("user", "Update user",
+                "string", true, "user1", RequestRouter.ParamDef.BODY);
+        route.parameters.add(parameter1);
+        route.parameters.add(parameter2);
+        route.parameters.add(parameter3);
 
         d.documentDescription.serviceRequestRoutes
                 .put(route.action, Collections.singletonList(route));
         return d;
+    }
+
+    private void handleGetForShort(Operation get) {
+        Token response = new Token();
+        response.token = String.valueOf(UUID.randomUUID().getMostSignificantBits());
+        get.setBody(response).complete();
     }
 }

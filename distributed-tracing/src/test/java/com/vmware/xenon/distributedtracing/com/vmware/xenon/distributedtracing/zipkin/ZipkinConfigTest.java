ea4c699799cfb1e4afb09da3940fbd398867eabd
Robert Collins <robertc@vmware.com>
2017-10-04 16:00:52 +1300

    Overhaul distributed-tracing to be Opentracing based.
    * Overhaul and update to OpenTracing rather than Brave v2 libraries.
    This is a major API incompatible break but should +  be unlikely to
    happen in future, as OpenTracing has passed 1.0 in its API and is very
    much looking for long term stability to enhance adoption.
    
    * Added support for Zipkin V2 API servers in addition to V1 servers.
    
    * Added support for Jaeger reporting.
    
    * Rewrite README to cover the new features and be more direct.
    
    Change-Id: Ide556c24c4df17d8e78c19094cfd0a6df835d0bf

---
+++ distributed-tracing/src/test/java/com/vmware/xenon/distributedtracing/com/vmware/xenon/distributedtracing/zipkin/ZipkinConfigTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017-2017 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.xenon.distributedtracing.com.vmware.xenon.distributedtracing.zipkin;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+
+import com.vmware.xenon.distributedtracing.DTracer;
+import com.vmware.xenon.distributedtracing.Helpers;
+import com.vmware.xenon.distributedtracing.IsolateConfig;
+
+public class ZipkinConfigTest {
+
+    @Rule
+    public ExternalResource isolateConfig = new IsolateConfig();
+
+    // TODO: These would be better if we introspected the resulting tracers
+    //       to be sure the config was set right. The API isn't convenient for that though.
+    @Test
+    public void testZipkinDriverAllProperties() throws Throwable {
+        System.setProperty("tracer.appName", "tracerhost1");
+        System.setProperty("tracer.implementation", "zipkin");
+        System.setProperty("tracer.sampleRate", "1");
+        System.setProperty("tracer.zipkinUrl", "http://zipkinUrl/api/v1/spans");
+        Helpers.runHost();
+    }
+
+    @Test
+    public void testSystemPropertiesNoSamplerRate() throws Throwable {
+        System.setProperty("tracer.appName", "tracerhost2");
+        System.setProperty("tracer.implementation", "zipkin");
+        System.setProperty("tracer.zipkinUrl", "http://zipkinUrl/api/v1/spans");
+        Helpers.runHost();
+    }
+
+    @Test(expected = DTracer.InvalidConfigException.class)
+    public void testMissingURLFatal() throws Throwable {
+        System.setProperty("tracer.appName", "missingurl");
+        System.setProperty("tracer.implementation", "zipkin");
+        DTracer.getTracer();
+    }
+}
